ebdc469c5dde3bc2067d16e177ea101b84d0ff47
小王同学 <9124422+wrt_136@user.noreply.gitee.com>
2024-11-10 22:39:42 +0800

    first commit

---
+++ src/main/java/org/example/car_back/CarBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.car_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/car_back/CarBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.car_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
